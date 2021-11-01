52184bf18069d3de598c06cb63c2cb186da89255
Rahul <rahulee19@gmail.com>
2021-11-01 21:20:13 +0530

    components, styles folder created

---
+++ mentalhealth/src/components/Landing.jsx
@@ -0,0 +1,13 @@
+import '../styles/landing.css'
+
+var maindiv = 'lpage1'
+
+export function Landing() {
+    return (
+        <>
+            <div
+                className={`${maindiv}`}
+            ></div>
+        </>
+    )
+}
