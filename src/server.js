389eb5541f945ae948d0c50b45f95474d2ea4d0f
Pratik Jadhav <pratikjadhav080@gmail.com>
2021-11-01 15:26:33 +0530

    added backend structure

--- src/server.js
+++
@@ -1,7 +0,0 @@
-const app = require("./index")
-const connect = require("./configs/db.js")
-
-app.listen(2345, async function () {
-    await connect();
-    console.log("listening on port 2345");
-})
