90bf828cc95fa9eecfdf986ea28e7c7091b6fad0
Pratik Jadhav <pratikjadhav080@gmail.com>
2021-11-02 12:59:26 +0530

    goodbadpagedone

--- mentalhealth/src/components/Goodbad.jsx
+++ mentalhealth/src/components/Goodbad.jsx
@@ -6,13 +6,14 @@ export const Goodbad = () => {
     return (
         <>
             <div id="goodbad">
-                <StaticHeader/>
+                <StaticHeader />
                 <p id="feeling">How are you feeling?</p>
                 <img id="good" src='good.jpg'></img>
                 <img id="bad" src='bad.jpg'></img>
-                <p>GOOD</p>
-                <p>BAD</p>
-                <button>Skip</button>
+                    <p id="goodtext">GOOD</p>
+                    <p id="badtext">BAD</p>
+
+                <button id="skip">Skip</button>
             </div>
         </>
     )
