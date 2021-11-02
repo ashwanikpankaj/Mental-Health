4eda001faeb7eb6b3a574d2870b4ba0265d1da44
ashwanikpankaj <ashnit8294@gmail.com>
2021-11-02 15:04:36 +0530

    welcome

--- mentalhealth/src/components/Welcome.jsx
+++ mentalhealth/src/components/Welcome.jsx
@@ -1,9 +1,10 @@
 
-import Vector from "./welcome/Vector.png"
-
-import "./welcome/welcome.css"
+import "../styles/welcome.css"
 
 import {useState} from "react";
+
+import { StaticHeader } from './Staticheader'
+
 function Welcome (){
 
     const [count,setCount] = useState(1);
@@ -12,13 +13,14 @@ function Welcome (){
     console.log(count)
 
     return  count == 1?(
-        <div className="cont">  
-        <div>
-        <img src={Vector}/> 
+        <div className="cont"> 
+        <StaticHeader/>
+        <div className="img-div">
+        <img src="welcome.png"/> 
         </div>
         <h1 className="welcome">Welcome</h1>
         <div className="bottom-box">
-         <div className="text">“Bring in the spirituality and positivity in you with Blue aura.“</div>
+         <div className="text">“Bring in the spirituality and positivity in you with Blue aura.”</div>
          <div className="button-cont">
            <div>
          <button className="skip">Skip</button>
@@ -35,12 +37,13 @@ function Welcome (){
         </div>
        
     ): count==2? ( <div className="cont">  
-    <div>
-    <img src={Vector}/> 
+     <StaticHeader/>
+    <div  className="img-div">
+    <img src="self-love.png"/> 
     </div>
-    <h1 className="welcome">Welcom</h1>
+    <h1 className="welcome">Self-Love</h1>
     <div className="bottom-box">
-     <div className="text">“Bring in the spirituality and positivity in you with Blue aura.“</div>
+     <div className="text">“Meditation helps to love with self and around too.”</div>
      <div className="button-cont">
        <div>
      <button className="skip">Skip</button>
@@ -49,20 +52,21 @@ function Welcome (){
       <div></div><div></div><div></div>
       </div>
       <div>
-      <button className="next" onClick={()=>setCount(count+1)} >Next</button>
+      <button className="next" onClick={()=>setCount(count+1)}  >Next</button>
       </div>
      </div>  
     </div>
     <div className="bottom-line"><div></div></div>
     </div>
     ):(
-        <div className="cont">  
-        <div>
-        <img src={Vector}/> 
+        <div className="cont"> 
+         <StaticHeader/> 
+        <div  className="img-div">
+        <img src="meditate.png"/> 
         </div>
-        <h1 className="welcome">Welcom3</h1>
+        <h1 className="welcome">Play & Meditate</h1>
         <div className="bottom-box">
-         <div className="text">“Bring in the spirituality and positivity in you with Blue aura.“</div>
+         <div className="text">“Easy cognitive games that will help you work on you mind positively.” </div>
          <div className="button-cont">
            <div>
          <button className="skip">Skip</button>
