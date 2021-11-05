53ce45ada84c3ad6804ab6f26e86d88e62635839
Pratik Jadhav <pratikjadhav080@gmail.com>
2021-11-05 11:29:17 +0530

    added some changes in server and removed  red-border from frontend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,16 +1,26 @@
 const express = require('express');
-
 const passport = require("./configs/passport")
-
+const session = require('express-session');
 const {register, login} = require("./controllers/authController")
 
 var cors = require('cors')
 
 const app = express();
 
-app.use(cors()) 
+// app.use(cors()) 
+app.use(cors({origin: 'http://localhost:3000', credentials: true}));
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(passport.initialize());
+app.use(passport.session());
+app.use(
+    session({
+        secret: 'secretcode',
+        resave: true,
+        saveUninitialized: true,
+    })
+)
+
 
 passport.serializeUser(function({user, token}, done) {
     done(null, {user, token});
