8e56103712e75b6b15080f83015b75a64cf56b0c
Pratik Jadhav <pratikjadhav080@gmail.com>
2021-11-09 00:02:08 +0530

    facebook authentication done

--- backend/src/configs/fbpassport.js
+++ backend/src/configs/fbpassport.js
@@ -1,60 +1,49 @@
 const FacebookStrategy = require('passport-facebook').Strategy;
-
 require("dotenv").config()
-
 const jwt = require("jsonwebtoken")
-
 const passport = require("passport");
-
 const User = require("../models/userModel")
+const { v4: uuidV4 } = require('uuid');
+const {newToken} = require("../controllers/authController")
 
-const {v4: uuidV4} = require('uuid');
-
-const newToken = (user)=>{
-   return jwt.sign({user},process.env.JWT_SECRET_KEY)
-}
+// const newToken = (user) => {
+//   return jwt.sign({ user }, process.env.JWT_SECRET_KEY)
+// }
 
 passport.use(new FacebookStrategy({
-    clientID: process.env.APP_ID,
-    clientSecret: process.env.APP_SECRET,
-    callbackURL: "http://localhost:7765/auth/facebook/callback",
-    profileFields: ['id', 'displayName', 'email']
-  },
-  async function(accessToken, refreshToken, profile, cb) {
-  
-//    console.log(profile)
- // profile exist
+  clientID: process.env.APP_ID,
+  clientSecret: process.env.APP_SECRET,
+  callbackURL: "http://localhost:7765/auth/facebook/callback",
+  profileFields: ['id', 'displayName', 'email']
+},
 
- 
+  async function (accessToken, refreshToken, profile, done) {
 
-const email = profile?._json?.email;
+    console.log("profile",profile)
+    const email = profile?._json?.email;
+    const name = profile?._json?.name;
+    let user;
 
- const name = profile?._json?.name;
+    try {
 
+      user = await User.findOne({ email }).lean().exec();
 
+      //------------if user doesnt exist then create the user-------------//
+      if (!user) {
+        user = await User.create({
+          name: name,
+          password: uuidV4(),
+          email: email
+        })
+      }
+      console.log("user", user)
+      const token = newToken(user)
+      return done(null, { user, token });
+    }
 
- let user;
-
- try{
-
-   user = await User.findOne({email}).lean().exec();
-
-  //------------if user doesnt exist then create the user-------------//
-  if(!user){
-    user = await User.create({
-        name:name,
-        password:uuidV4(),
-        email:email
-    })
-  }
- console.log("user",user)
-  const token = newToken(user)
-    return cb(null, {user,token});
- }
-
-   catch(err){
-       console.log(err)
-   }
+    catch (err) {
+      console.log(err)
+    }
 
   }
 ));
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,48 +1,37 @@
 
 const express = require('express');
-<<<<<<< HEAD
-
 const app = express();
-
 const passport = require("./configs/passport")
+const session = require('express-session');
+const cors = require('cors')
+
 
 //---------------fb auth----------//
 const fbpassport = require("./configs/fbpassport");
 
-app.use(fbpassport.initialize());
+app.use(session({ secret: 'ilovescotchscotchyscotchscotch' }));
+app.use(passport.initialize());
 
 passport.serializeUser(function({user, token}, done) {
     done(null, {user, token});
 });
   
 passport.deserializeUser(function({user, token}, done) {
-    done(err, {user, token});
+    done(null, {user, token});
 });
 
 
 app.get('/auth/facebook',
-  passport.authenticate('facebook'));
+  passport.authenticate('facebook',{ scope : 'email,user_photos' }));
 
 app.get('/auth/facebook/callback',
   passport.authenticate('facebook', { failureRedirect: '/login' }),
   function(req, res) {
-    // Successful authentication, redirect home.
-    // console.log("hello")
-   //  res.redirect('http://localhost:3000');
-    return res
-    .status(200)
-    .send({ user1: req.user.user, token: req.user.token })
+   res.redirect('http://localhost:3000/blueaura');
   });
 //----------------------------//
 
-=======
-const passport = require("./configs/passport")
-const session = require('express-session');
->>>>>>> e901b095a44a4fa35c29f9196b19cc2ea297a84d
 const {register, login} = require("./controllers/authController")
-
-const cors = require('cors')
-
 
 
 // app.use(cors()) 
@@ -86,19 +75,26 @@ app.get( '/auth/google/callback',
     passport.authenticate( 'google', {
         failureRedirect: '/auth/google/failure'
 }), function(req, res) {
-    // const {user, token} = req.user
-    // return res.status(200).json({user, token });
     res.redirect('http://localhost:3000/blueaura')
 });
-
 
 
 app.post("/register", register);
 app.post("/login", login);
 
-app.get('/getuser', (req, res) => {
+app.get('/profile', isLoggedIn,(req, res) => {
     res.send(req.user);
 })
+
+function isLoggedIn(req, res, next) {
+
+    // if user is authenticated in the session, carry on
+    if (req.isAuthenticated())
+        return next();
+
+    // if they aren't redirect them to the home page
+    res.redirect('http://localhost:3000');
+}
 
 
 module.exports = app;
