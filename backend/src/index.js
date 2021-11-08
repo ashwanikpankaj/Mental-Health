13ce9fd9b4dc364b6f636a6c232fd0c591b0ccd3
Pratik Jadhav <pratikjadhav080@gmail.com>
2021-11-09 00:21:27 +0530

    facebook authentication refractored

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,40 +1,11 @@
-
 const express = require('express');
 const app = express();
 const passport = require("./configs/passport")
 const session = require('express-session');
 const cors = require('cors')
-
-
-//---------------fb auth----------//
+const {register, login} = require("./controllers/authController")
 const fbpassport = require("./configs/fbpassport");
 
-app.use(session({ secret: 'ilovescotchscotchyscotchscotch' }));
-app.use(passport.initialize());
-
-passport.serializeUser(function({user, token}, done) {
-    done(null, {user, token});
-});
-  
-passport.deserializeUser(function({user, token}, done) {
-    done(null, {user, token});
-});
-
-
-app.get('/auth/facebook',
-  passport.authenticate('facebook',{ scope : 'email,user_photos' }));
-
-app.get('/auth/facebook/callback',
-  passport.authenticate('facebook', { failureRedirect: '/login' }),
-  function(req, res) {
-   res.redirect('http://localhost:3000/blueaura');
-  });
-//----------------------------//
-
-const {register, login} = require("./controllers/authController")
-
-
-// app.use(cors()) 
 app.use(cors({origin: 'http://localhost:3000', credentials: true}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -46,7 +17,6 @@ app.use(
         saveUninitialized: true,
     })
 )
-
 app.use(passport.initialize());
 app.use(passport.session());
 
@@ -59,17 +29,29 @@ passport.deserializeUser(function({user, token}, done) {
 });
 
 
+//---------------fb auth----------//
+
+app.get('/auth/facebook',
+  passport.authenticate('facebook',{ scope : 'email' }));
+
+app.get('/auth/facebook/callback',
+  passport.authenticate('facebook', { failureRedirect: '/login' }),
+  function(req, res) {
+   res.redirect('http://localhost:3000/blueaura');
+  });
+
+//--------------------------------//
+
+//---------------google auth----------//
+
 app.get("/auth/google/failure", function(req, res) {
     return res.send("Something went wrong");
 })
-
 
 app.get('/auth/google',
   passport.authenticate('google', { scope:
       [ 'email', 'profile' ] }
 ));
-
-
 
 app.get( '/auth/google/callback',
     passport.authenticate( 'google', {
@@ -78,23 +60,19 @@ app.get( '/auth/google/callback',
     res.redirect('http://localhost:3000/blueaura')
 });
 
+//--------------------------------//
+
 
 app.post("/register", register);
 app.post("/login", login);
-
 app.get('/profile', isLoggedIn,(req, res) => {
     res.send(req.user);
 })
 
 function isLoggedIn(req, res, next) {
-
-    // if user is authenticated in the session, carry on
     if (req.isAuthenticated())
         return next();
-
-    // if they aren't redirect them to the home page
     res.redirect('http://localhost:3000');
 }
-
 
 module.exports = app;
