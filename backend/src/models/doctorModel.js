5de7483c9b983ae93f9273079f654d24ba584bf5
Pratik Jadhav <pratikjadhav080@gmail.com>
2021-11-09 12:07:05 +0530

    added categorymodel and categorycontroller

--- backend/src/models/doctorModel.js
+++ backend/src/models/doctorModel.js
@@ -4,7 +4,11 @@ let doctorSchema = new mongoose.Schema({
     name: { type: String, trim:true, required: true },
     photo:{ type: String, trim:true, required: true },
     qualification:{ type: String, trim:true, required: true },
-    specialization:[{ type: String, trim:true, required: true }],
+    specialization:[{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "category",
+        required: true,
+      }], 
     intro:{ type: String, trim:true, required: true },
     ratings:{type: Number, trim:true, required: true}
 },
