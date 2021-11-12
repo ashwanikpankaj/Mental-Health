94405e82522dc40267674a8dbfcf0533b21958f1
ashwanikpankaj <ashnit8294@gmail.com>
2021-11-12 21:14:46 +0530

    heading time

--- mentalhealth/src/components/Staticheader.jsx
+++ mentalhealth/src/components/Staticheader.jsx
@@ -2,30 +2,20 @@ import { useState } from "react";
 import "../styles/staticHeader.css";
 
 export const StaticHeader = () => {
-  const today = new Date();
-  let h = today.getHours();
-  let m = today.getMinutes();
-  checkTime(h);
-  checkTime(m);
+ let time = new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })
+ const [ctime,setCTime] = useState(time);
 
-  function checkTime(i) {
-    if (i < 10) {
-      i = "0" + i;
-    }
-    return i;
-  }
-  const [time, setTime] = useState(`${h}:${m}`);
+ const Updatetime = ()=>{
+   time  = new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit',});
+   setCTime(time)
+ }
 
-  const updateTime = () => {
-    setTime(`${h}:${m}`);
-  };
-
-  setInterval(updateTime, 1000);
+  setInterval(Updatetime,1000)
 
   return (
     <>
       <div className="headerBox">
-        <p id="time">{time}</p>
+        <p id="time">{ctime}</p>
         <img id="wifi" src="/wifi.jpg" alt="alt"></img>
         <img id="tower" src="/tower.jpg" alt="alt"></img>
         <img id="battery" src="/battery.jpg" alt="alt"></img>
